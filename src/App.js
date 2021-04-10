7100a7150c3ec355eb52562990f5415dba1b9c4b
Muhammet <muhammet@affexgroup.com>
2021-04-10 16:24:39 +0300

    Touchbar created

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
-import React, { useEffect, useRef, useState } from 'react';
-
-const TouchMenuItems = ({ onHandleClickGrabBoxItem }) => {
-  let [isShow, setIsShow] = useState(false)
-  useEffect(() => {
-    let timeout = setTimeout(() => setIsShow(true), 250)
-    return () => clearTimeout(timeout)
-  }, [])
-
-  return (
-    <>
-      <div className="grab-box__modal" onClick={e => onHandleClickGrabBoxItem(e,0)} />
-      <div className={`grab-box__grid${isShow ? " opacity" : ""}`}>
-        <div className="grab-box__item">
-          <button onClick={(e) => onHandleClickGrabBoxItem(e, 1)}>
-            Hello
-          </button>
-        </div>
-        <div className="grab-box__item">
-          <button onClick={(e) => onHandleClickGrabBoxItem(e, 2)}>
-            Hello
-          </button>
-        </div>
-        <div className="grab-box__item">
-          <button onClick={(e) => onHandleClickGrabBoxItem(e, 3)}>
-            Hello
-          </button>
-        </div>
-        <div className="grab-box__item">
-          <button onClick={(e) => onHandleClickGrabBoxItem(e, 4)}>
-            Hello
-          </button>
-        </div>
-      </div>
-    </>
-  )
-}
-
+import React, { useRef, useState } from 'react';
+import TouchMenuItems from './TouchMenuItems';
 
 const TouchMenu = () => {
-  let [styles, setStyles] = useState({ top: 100, left: 200, width: 60, height: 60 });
-  let menuIsOpen = useRef(false)
-  let isDragged = useRef(false);
-  let prevPositions = useRef({ top: null, left: null });
   let INNER_WIDTH = window.innerWidth;
   let INNER_HEIGHT = window.innerHeight;
+  let TRANSITION_NORMAL = "all .25s linear";
+  let TRANSITION_ZERO = "all 0s linear";
+  let menuIsOpen = useRef(false);
+  let isDragged = useRef(false);
+  let prevPositions = useRef({ top: INNER_HEIGHT - 70, left: 10 });
+
+  let [styles, setStyles] = useState(() => {
+    let initCoords = JSON.parse(localStorage.getItem("TouchMenuCoords"));
+    if (initCoords === null) return { top: INNER_HEIGHT - 70, left: 10, width: 60, height: 60 };
+    let { left0, top0 } = initCoords
+    return { top: top0, left: left0, width: 60, height: 60 }
+  });
+
+  function positionAutoControl(resolve) {
+    let LEFT = (INNER_WIDTH - 60) / 2;
+    let TOP = (INNER_HEIGHT - 60) / 2;
+    let { left, top } = styles;
+    if (left <= LEFT && top <= TOP) {
+      if (left > top) {
+        setStyles(prevStyles => ({ ...prevStyles, top: 0, transition: TRANSITION_NORMAL }))
+        return
+      }
+      setStyles(prevStyles => ({ ...prevStyles, left: 0, transition: TRANSITION_NORMAL }))
+    } else if (left > LEFT && top <= TOP) {
+      if (INNER_WIDTH - left - 60 > top) {
+        setStyles(prevStyles => ({ ...prevStyles, top: 0, transition: TRANSITION_NORMAL }))
+        return
+      }
+      setStyles(prevStyles => ({ ...prevStyles, left: INNER_WIDTH - 60, transition: TRANSITION_NORMAL }))
+    } else if (left <= LEFT && top > TOP) {
+      if (left > INNER_HEIGHT - top - 60) {
+        setStyles(prevStyles => ({ ...prevStyles, top: INNER_HEIGHT - 60, transition: TRANSITION_NORMAL }))
+        return
+      }
+      setStyles(prevStyles => ({ ...prevStyles, left: 0, transition: TRANSITION_NORMAL }))
+    } else if (left > LEFT && top > TOP) {
+      if (INNER_WIDTH - left > INNER_HEIGHT - top) {
+        setStyles(prevStyles => ({ ...prevStyles, top: INNER_HEIGHT - 60, transition: TRANSITION_NORMAL }))
+        return
+      }
+      setStyles(prevStyles => ({ ...prevStyles, left: INNER_WIDTH - 60, transition: TRANSITION_NORMAL }))
+    }
+    resolve()
+  }
 
   function setPositionToBox(event, distanceX, distanceY) {
     isDragged.current = true;
@@ -54,39 +58,68 @@ const TouchMenu = () => {
   }
 
   function onHandleTouchStart(event) {
-    setStyles(prevStyles => ({ ...prevStyles, transition: "all 0s linear" }));
-    // Distance box coord's until touched place coord's 
+    setStyles(prevStyles => ({ ...prevStyles, transition: TRANSITION_ZERO }));
     const distanceX = event.changedTouches[0].pageX - styles.left;
     const distanceY = event.changedTouches[0].pageY - styles.top;
     if (!menuIsOpen.current) {
       document.ontouchmove = e => setPositionToBox(e, distanceX, distanceY);
-    } else {
-      isDragged.current = true;
+      return
     }
+    isDragged.current = true;
   }
 
   function onHandleTochEnd() {
     document.ontouchmove = () => null;
     if (isDragged.current) {
       isDragged.current = false;
-    } else {
-      menuIsOpen.current = true;
-      if (menuIsOpen.current) {
-        prevPositions.current = { left: styles.left, top: styles.top };
-        setStyles({ transition: "all .25s linear", width: 300, height: 300, left: (INNER_WIDTH - 300) / 2, top: (INNER_HEIGHT - 300) / 2 });
+      if (!menuIsOpen.current) {
+        new Promise(resolve => {
+          positionAutoControl(resolve)
+        }).then(() => setStyles(prevStyles => ({ ...prevStyles, transition: TRANSITION_ZERO })))
       }
+      return
+    }
+    menuIsOpen.current = true;
+    if (menuIsOpen.current) {
+      prevPositions.current = { left: styles.left, top: styles.top };
+      setStyles({ transition: TRANSITION_NORMAL, width: 300, height: 300, left: (INNER_WIDTH - 300) / 2, top: (INNER_HEIGHT - 300) / 2 });
     }
   }
 
-  function onHandleClickGrabBoxItem(e, number) {
+  function onHandleClickGrabBoxItem(e, type) {
     e.stopPropagation();
     menuIsOpen.current = false;
-    setStyles({ transition: "all .25s linear", width: 60, height: 60, left: prevPositions.current.left, top: prevPositions.current.top });
-    console.log(number)
+    setStyles({ transition: TRANSITION_NORMAL, width: 60, height: 60, left: prevPositions.current.left, top: prevPositions.current.top });
+    switch (type) {
+      case "Menu":
+        console.log("Menu")
+        break;
+      case "Casino":
+        console.log("Casino")
+        break;
+      case "Search":
+        console.log("Search")
+        break;
+      case "Home":
+        console.log("Home")
+        break;
+      case "Chat":
+        console.log("Chat")
+        break;
+      default:
+        break
+    }
   }
 
+  window.onunload = () => {
+    if (menuIsOpen.current) {
+      localStorage.setItem("TouchMenuCoords", JSON.stringify({ left0: prevPositions.current.left, top0: prevPositions.current.top }))
+      return
+    }
+    localStorage.setItem("TouchMenuCoords", JSON.stringify({ left0: styles.left, top0: styles.top }))
+  }
   return (
-    <div className="grab-box"
+    <div className="touchbar"
       style={styles}
       onTouchStart={onHandleTouchStart}
       onTouchEnd={onHandleTochEnd}
@@ -94,11 +127,13 @@ const TouchMenu = () => {
       {
         menuIsOpen.current
           ?
-          <TouchMenuItems onHandleClickGrabBoxItem={onHandleClickGrabBoxItem} />
+          <TouchMenuItems
+            onHandleClickGrabBoxItem={onHandleClickGrabBoxItem}
+          />
           :
           <>
-            <div className="grab-box_radius1">
-              <div className="grab-box_radius2">
+            <div className="touchbar_radius1">
+              <div className="touchbar_radius2">
               </div>
             </div>
           </>
---
+++ src/TouchMenuItems.js
@@ -0,0 +1,49 @@
+import { faBars, faDice, faHome, faSearch, faSms } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+
+const TouchMenuItems = ({ onHandleClickGrabBoxItem }) => {
+    let [isShow, setIsShow] = useState(false)
+    useEffect(() => {
+        let timeout = setTimeout(() => setIsShow(true), 300)
+        return () => clearTimeout(timeout)
+    }, [])
+
+    return (
+        <>
+            <div className="touchbar__modal" onClick={e => onHandleClickGrabBoxItem(e, "Modal")} />
+            <div className={`touchbar__grid${isShow ? " opacity" : ""}`}>
+                <div className="touchbar__item">
+                    <button onClick={(e) => onHandleClickGrabBoxItem(e, "Menu")}>
+                        <FontAwesomeIcon size="lg" icon={faBars} />
+                        <div>Menu</div>
+                    </button>
+                </div>
+
+                <div className="touchbar__item">
+                    <button onClick={(e) => onHandleClickGrabBoxItem(e, "Casino")}>
+                        <FontAwesomeIcon size="lg" icon={faDice} />
+                        <div>Casino</div>
+                    </button>
+                </div>
+
+                <div className="touchbar__item">
+                    <button onClick={(e) => onHandleClickGrabBoxItem(e, "Search")}>
+                        <FontAwesomeIcon size="lg" icon={faSearch} />
+                        <div>Search</div>
+                    </button>
+                </div>
+
+                <div className="touchbar__item">
+                    <button onClick={(e) => onHandleClickGrabBoxItem(e, "Home")}>
+                        <FontAwesomeIcon size="lg" icon={faHome} />
+                        <div>Home</div>
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default TouchMenuItems;
